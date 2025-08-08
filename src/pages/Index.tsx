d8c80e7f5c4b5101ac4cae560f7c8aaf539b4e7e
Utsav gautam <utsav.gautam2024@vitstudent.ac.in>
2025-08-08 10:53:08 +0530

    Remove View Demo button from landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,6 @@ import {
   Package,
   Globe,
   Lock,
-  BarChart3,
   Sparkles,
   Rocket,
   Target,
@@ -263,14 +262,7 @@ const Index = () => {
                 <Rocket className="w-5 h-5 mr-2" />
                 Start Free Trial
               </Button>
-              <Button 
-                variant="outline" 
-                size="lg"
-                className="border-2 border-white text-white hover:bg-white hover:text-blue-600 px-8 py-4 text-lg font-semibold transition-all duration-300 hover:scale-105"
-              >
-                <BarChart3 className="w-5 h-5 mr-2" />
-                View Demo
-              </Button>
+
             </div>
           </div>
         </div>
